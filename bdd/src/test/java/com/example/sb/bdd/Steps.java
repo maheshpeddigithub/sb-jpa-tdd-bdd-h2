5870a7929387632a06dca2778b0953b20524b29f
Mahesh Peddi <maheshbabu.peddi@googlecloud.corp-partner.google.com>
2023-02-01 19:31:45 +0000

    BDD refactor

--- bdd/src/test/java/com/example/sb/bdd/Steps.java
+++ bdd/src/test/java/com/example/sb/bdd/Steps.java
@@ -1,38 +1,87 @@
 package com.example.sb.bdd;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class Steps {
 
     private static final String BASE_URL = "http://localhost:8080/api/author";
 
-    private static RequestSpecification request;
     private static Response response;
+    private static Author author = null;
 
-    @Given("create author request prepared")
-    public void createAuthorRequestPrepared() {
-        RestAssured.baseURI = BASE_URL;
-        request = RestAssured.given();
-
+    @Given("create author request received")
+    public void createAuthorRequestReceived() {
     }
 
     @When("I place request for create author")
-    public void iPlacedRequestForCreateAuthor() throws JsonProcessingException {
-        request.header("Content-Type", "application/json");
-        response = request.body((new ObjectMapper()).writeValueAsString(Author.builder().name("n1").build())).post();
-        response = request.get();
+    public void iPlaceRequestForCreateAuthor() throws JsonProcessingException {
+        response = given().header("Content-Type", "application/json").body((new ObjectMapper()).writeValueAsString(Author.builder().name("n1").build())).post(BASE_URL);
     }
 
     @Then("author is created")
-    public void authorIsAdded() {
-        String jsonString = response.asString();
+    public void authorIsCreated() {
+        assertEquals(201, response.statusCode());
+    }
+
+
+    @Given("get author details request received")
+    public void getAuthorDetailsRequestReceived() {
+    }
+
+    @When("I place request for get author details")
+    public void iPlaceRequestForGetAuthorDetails() {
+        response = given().header("Content-Type", "application/json").get(BASE_URL);
+    }
+
+    @Then("author details are retrieved")
+    public void authorDetailsAreRetrieved() throws JsonProcessingException {
+        assertEquals(200, response.statusCode());
+        List<Author> authors = (new ObjectMapper()).readValue(response.getBody().asString(), new TypeReference<List<Author>>(){});
+        author = authors.stream().findFirst().orElse(null);
+        assertEquals("n1", author.getName());
+    }
+
+    @Given("update author details request received")
+    public void updateAuthorDetailsRequestReceived() {
+    }
+
+    @When("I place request for update author details")
+    public void iPlaceRequestForUpdateAuthorDetails() throws JsonProcessingException {
+        response = given().header("Content-Type", "application/json").body((new ObjectMapper()).writeValueAsString(Author.builder().name("n11").build())).patch(BASE_URL + "/" + author.getId());
+    }
+
+    @Then("author details are updated")
+    public void authorDetailsAreUpdated() throws JsonProcessingException {
+        assertEquals(202, response.statusCode());
+        response = given().header("Content-Type", "application/json").get(BASE_URL + "/" + author.getId());
+        assertEquals(200, response.statusCode());
+        Author author = (new ObjectMapper()).readValue(response.getBody().asString(), Author.class);
+        assertEquals("n11", author.getName());
+    }
+
+    @Given("delete author request received")
+    public void deleteAuthorDetailsRequestReceived() {
+    }
+
+    @When("I place request for delete author")
+    public void iPlaceRequestForDeleteAuthor() {
+        response = given().header("Content-Type", "application/json").delete(BASE_URL + "/" + author.getId());
+    }
+
+    @Then("author is deleted")
+    public void authorIsDeleted() {
+        assertEquals(202, response.statusCode());
     }
 
 }
--- bdd/src/test/resources/sb-jpa-tdd-bdd-h2-bdd.feature
+++ bdd/src/test/resources/sb-jpa-tdd-bdd-h2-bdd.feature
@@ -1,6 +1,21 @@
 Feature: BDDs for sb-jpa-tdd-bdd-h2
 
-    Scenario: Create Author record
-      Given create author request prepared
-      When I place request for create author
-      Then author is created
+  Scenario: Create Author record
+    Given create author request received
+    When I place request for create author
+    Then author is created
+
+  Scenario: Get Author details
+    Given get author details request received
+    When I place request for get author details
+    Then author details are retrieved
+
+  Scenario: Update Author details
+    Given update author details request received
+    When I place request for update author details
+    Then author details are updated
+
+  Scenario: Delete Author
+    Given delete author request received
+    When I place request for delete author
+    Then author is deleted
